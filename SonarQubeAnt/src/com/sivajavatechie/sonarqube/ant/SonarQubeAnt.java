a4968939ccf79a51d28643c60836562b8e466dd0
sivareddy <sivareddy@gmail.com>
2018-06-22 10:26:53 +0530

    sonarqube scanner for Ant Project

---
+++ SonarQubeAnt/src/com/sivajavatechie/sonarqube/ant/SonarQubeAnt.java
@@ -0,0 +1,16 @@
+package com.sivajavatechie.sonarqube.ant;
+
+
+public class SonarQubeAnt {
+
+	public static void main(String[] args) {
+
+		System.out.println("Welcome to Ant Project");
+		displayMessage();		
+	}
+		
+	public static void displayMessage() {
+		System.out.println("Welcome to SonarQube Report for Ant Project");
+	}
+
+}
