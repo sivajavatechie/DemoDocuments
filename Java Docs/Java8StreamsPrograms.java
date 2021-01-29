d66500f84dae28edb796810760208c7524ef756b
siva <sivajavatechie5@gmail.com>
2021-01-29 01:04:41 -0500

    Frequently asked coding programs on Java 8 Streams concept with explaination on Youtube

--- Java Docs/Java8StreamsPrograms.java
+++ Java Docs/Java8StreamsPrograms.java
@@ -15,7 +15,8 @@ import java.util.stream.Collectors;
  * You can run this program on any Java IDE(eclipse, IntelliJ etc..)
  * Create Java project, create package with name com.sivajavatechie.streams
  * and Create a Class name with Java8StreamsPrograms and copy this code and run the program as Java Application
- *
+ * If you are looking for an explanation on the below programs, Please refer below youtube link.
+ * Youtube URL: https://www.youtube.com/watch?v=1pweZskNq7w
  */
 
 public class Java8StreamsPrograms {
@@ -30,7 +31,7 @@ public class Java8StreamsPrograms {
 		empList.add(new Employee(105, "bob", 103, "active", 3500));
 		empList.add(new Employee(106, "alice", 103, "inactive", 3500));
 		empList.add(new Employee(107, "srinu", 104, "active", 3500));
-
+		//1.Write a program to print employee details working in each department
 		System.out.println("========== Get Employee details department wise ==========");
 
 		Map<Integer, Set<Employee>> empMap = empList.stream()
@@ -39,26 +40,29 @@ public class Java8StreamsPrograms {
 			System.out.println(t.getKey() + "---->"
 					+ t.getValue().stream().map(v -> v.getEmpName()).collect(Collectors.joining(",")));
 		});
-
+		
+		//2.Write a program to print employees count working in each department
 		System.out.println("====== Employees count department wise =============");
 		Map<Integer, Long> empCountDeptWise = empList.stream()
 				.collect(Collectors.groupingBy(Employee::getDeptId, Collectors.counting()));
 		System.out.println(empCountDeptWise);
-
+	
+		//3.Write a program to print active and inactive employees in the given collection
 		System.out.println("================ Active and Inactive Employees count ================");
-
 		Long activeEmpCount = empList.stream().filter(t -> "active".equals(t.getStatus())).count();
 		Long inactiveEmpCount = empList.stream().filter(t -> "inactive".equals(t.getStatus())).count();
 		System.out.println("Active Employees count --> " + activeEmpCount);
 		System.out.println("InActive Employees count --> " + inactiveEmpCount);
 
+		//4.Write a program to print Max/Min employee salary from the given collection
 		System.out.println("================ Get Max/Min employee salary from the given collection ===========");
-
 		Optional<Employee> e = empList.stream().max(Comparator.comparing(Employee::getSalary));
 		System.out.println("Max salary employee details -->" + e);
 		e = empList.stream().min(Comparator.comparing(Employee::getSalary));
 		System.out.println("Min salary employee details -->" + e);
 
+
+		//5.Write a program to print the max salary of an employee from each department
 		System.out.println("=========== Max salary of an employee in each department ======");
 
 		Map<Integer, Optional<Employee>> topSalaryEmpDeptWise = empList.stream()
